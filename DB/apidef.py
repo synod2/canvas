cdef1e2732f1cf48e6022868cea6b64898bf2eb0
Kang <synod2@naver.com>
2017-10-19 11:34:49 +0000

    파이썬 코드 추가

--- DB/apidef.py
+++ DB/apidef.py
@@ -1,7 +1,7 @@
 import requests
 import json 
 
-api_key = "?api_key="+"RGAPI-08c8a6ef-d288-454a-9efe-798590b2cb2a"
+api_key = "?api_key="+"RGAPI-9bee6697-737e-429e-ad3e-4d54731c059c"
 
 
 def RetDataFromUrl(URL) : #URL로 호출한 페이지의 데이터를 텍스트 형태로 출력 후 json 데이터를 파싱 후 리턴.
@@ -14,5 +14,8 @@ def url_user(username) :  #소환사명 기반 소환사 정보 출력 url
 def url_RecMatchByID(accID) : #소환사 account id 기반 최근 20매치 출력 url 
         ret_url = "https://kr.api.riotgames.com/lol/match/v3/matchlists/by-account/"+accID+api_key
         return ret_url
-        
+
+def url_GetMatchByID(matchID) : 
+        ret_url = "https://kr.api.riotgames.com/lol/match/v3/matches/"+matchID+api_key
+        return ret_url
         
--- DB/crwaling.py
+++ DB/crwaling.py
@@ -1,11 +1,60 @@
 import apidef
 #import urllib2
 
-def ret_url(URL) :
+class matchPlayer(): 
+        
+        def __init__(self, partId):
+                self.partId = partId  
+        
+        accId = 0
+        sumName = ''
+        champ = 0
+
+
+def ret_url(URL) : #url로 페이지 호출시 텍스트 형식으로 리턴 
         return apidef.RetDataFromUrl(URL)
 
+     
+        
+def GetMatchData(matchID) : 
+        URL = apidef.url_GetMatchByID(matchID)
+        parsed = ret_url(URL)
+        cnt=0
+        
+        print ("매치 id : "+str(matchID))
+        
+        players = [ matchPlayer(i) for i in range(10) ]
+        
+        for data in parsed['participants'] : 
+                cnt = cnt+1 
+                #players = matchPlayer(cnt)
+                players.champ = data['championId']
+                
+        print (players.partId)        
+        
+        cnt=0
+        for data in parsed['participantIdentities'] :
+                cnt = cnt+1
+                p_SummonerName = str(data['player']['summonerName'])
+                p_AccountId = str(data['player']['accountId'])
+                print (str(cnt)+" 번째 소환사명 : "+p_SummonerName+", 계정 ID : "+p_AccountId)
+                
+                
 
-def userdata(username) : 
+def GetGameId(userid) : #유저 account ID를 받아서 해당 유저의 gid 출
+        
+        URL = apidef.url_RecMatchByID(userid)
+        parsed = ret_url(URL)
+        cnt = 0
+        
+        for data in parsed['matches'] : 
+                cnt = cnt+1
+                p_gid = str(data['gameId'])
+                print (str(cnt)+"번쨰 게임 gid : "+p_gid)
+                
+        GetMatchData(p_gid)
+ 
+def GetUserdata(username) : #소환사 명을 가지고 유저 accountID 받아오는 api
         URL = apidef.url_user(username)
         parsed = ret_url(URL)
         
@@ -15,10 +64,10 @@ def userdata(username) :
         print ("소환사명 : "+p_name)
         print ("Account id : "+p_id)
         
-        URL = apidef.url_RecMatchByID(p_id)
-        parsed = ret_url(URL)
-        print (parsed)
+        GetGameId(p_id)
+      #  print (parsed)
+   
+                
         
-        
-userdata("안인읒")
+GetUserdata("안인읒")
         
