186f25dea537465cbc904af43a7c59d4820bde02
Kang <synod2@naver.com>
2017-10-28 12:09:11 +0000

    코드 1차 수정

--- DB/apidef.py
+++ DB/apidef.py
@@ -6,6 +6,7 @@ api_key = "?api_key="+"RGAPI-d6241dc2-d2a7-4731-aba8-073325c9e332"
 '''
 def RetDataFromUrl(URL) : #URL로 호출한 페이지의 데이터를 텍스트 형태로 출력 후 json 데이터를 파싱 후 리턴.
         return json.loads(requests.get(URL).text)
+        #필요 없는 부분, 간략화
 '''
 #함수별로 필요한 데이터를 파라미터로 사용하면, 해당 API를 통하여 받아온 데이터를 파싱해서 
 #ret하는 함수들
